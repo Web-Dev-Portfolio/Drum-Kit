e567d81e6448a0a2d2cdb08782ed7dd6ce1f8c4f
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-06 02:26:05 +0700

    click behaviour for button

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-//alert("Testing");
+var allDrumButton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < allDrumButton; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){alert("I'm clicked!")});
+}
